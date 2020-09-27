ed644ab9c763280a8eede058598060f7e52cc587
Kemal MUTLU <mutlukemal1717@gmail.com>
2020-09-27 16:34:08 +0300

    Until loop kullanilarak, kullanicidan sayi alindi ve tahmin etmesi icin kosullar yazildi.

---
+++ game.rb
@@ -0,0 +1,19 @@
+puts 'Welcome to the guessing game!'
+number = rand(0..50)
+
+point = 25
+
+puts 'Pick any number between 0 and 50'
+user_number = gets.to_i
+
+until number == user_number
+	if(user_number < number)
+		puts "My number greather than #{user_number}. Your point is #{point}. Pick any number between 0 and 50"
+	elsif(number > user_number) 
+		puts "My number less than #{user_number}. Your point is #{point}. Pick any number between 0 and 50"
+	end
+		point -= 1
+		user_number = gets.to_i
+end
+	
+puts "You knew it. Your total point is #{point}"
